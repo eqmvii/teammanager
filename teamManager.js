18754cde21b874b7aff077e1b41f6563206b0de5
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-10 20:45:01 -0400

    working game; first draft done

--- teamManager.js
+++ teamManager.js
@@ -74,6 +74,7 @@ function makeAPlayer() {
                 playersArray.offense += playersArray[i].offense;
                 playersArray.defense += playersArray[i].defense;
             }
+            playersArray[2].printStats();
             // console.log("Players array: ");
             // console.log(playersArray);
             // console.log(playersArray.length);
@@ -85,10 +86,11 @@ function makeAPlayer() {
 
 function playGame() {
     var swapPlayers = false;
-    console.log("Time to play a game!");
+    console.log("\n\n");
+    console.log(`${playersArray[0].name} and ${playersArray[1].name} play a game!`);
     var randOne = Math.floor(Math.random() * 20) + 1;
     var randTwo = Math.floor(Math.random() * 20) + 1;
-    console.log(`1: ${randOne}; 2: ${randTwo}`);
+    // console.log(`1: ${randOne}; 2: ${randTwo}`);
     if (randOne < playersArray.offense) {
         console.log("Offense was good enough, +1 point!");
         playersArray.score++;
@@ -99,31 +101,99 @@ function playGame() {
         playersArray.score--;
         swapPlayers = true;
     }
-    inquirer.prompt([{
-        type: "list",
-        name: "pos",
-        message: "swap a player?",
-        choices: ["0", "1", "Do not substitute"]
-    }]).then(ans => {
-        console.log(ans);
-        console.log("Next game starts in 2 seconds");
-        if (playersArray.gamesPlayed < 5) {
-            playersArray.gamesPlayed++;
-            setTimeout(playGame, 2 * 1000);
-        } else {
-            console.log(`Score: ${playersArray.score}`);
+    if (!swapPlayers) {
+        console.log("No score so no player swap chance...");
+        finishGame();
+    } else { // TODO: Remove the prompt to swap players on the last game
+        inquirer.prompt([{
+            type: "list",
+            name: "pos",
+            message: "Swap a player for an alternate?",
+            choices: ["0", "1", "Do not substitute"]
+        }]).then(ans => {
+            // console.log(ans);
+            if (ans.pos === "Do not substitute") {
+                console.log("OK, not substituting");
+            } else {
+                swapPlayer(parseInt(ans.pos, 10));
+            }
+            finishGame();
+        })
+    }
+}
+
+function finishGame() {
+    console.log("Next game starts in 1 second");
+    if (playersArray.gamesPlayed < 5) {
+        playersArray.gamesPlayed++;
+        setTimeout(playGame, 1 * 1000);
+    } else {
+        console.log(`Five games have been played! Score: ${playersArray.score}`);
+        if (playersArray.score > 0) {
+            console.log("Good game! Starter stats go UP!");
+            for (let i = 0; i < playersArray.length - 1; i++) {
+                playersArray[i].goodGame();
+            }
         }
-    })
+        else if (playersArray.score < 0) {
+            console.log("TERRIBLE GAME! Starter stats go DOWN!");
+            for (let i = 0; i < playersArray.length - 1; i++) {
+                playersArray[i].badGame();
+            }
+        }
+        else {
+            console.log("Mediocre game. Starts stay the same");
+        }
+        inquirer.prompt([{
+            type: "list",
+            name: "again",
+            message: "Play again?",
+            choices: ["yes", "no"]
+        }]).then(ans => {
+            if (ans.again === "yes") {
+                // reset (move this logic)
+                playersArray = [];
+                playersArray.score = 0; // let's give the array a property. NEVER DO THIS.
+                playersArray.gamesPlayed = 0; // let's give the array a property. NEVER DO THIS.
+                makeAPlayer();
+            } else {
+                console.log("G A M E  O V E R");
+                console.log("G A M E  O V E R");
+                console.log("G A M E  O V E R");
+            }
+        })
+
+
+    }
+
+}
+
+function swapPlayer(index) {
+    console.log(`Swapping sub for player in index ${index}`);
+    var tempPlayer = playersArray[index];
+    playersArray[index] = playersArray[2];
+    playersArray[2] = tempPlayer;
+    // TODO: this is not DRY refactor
+    for (let i = 0; i < playersArray.length - 1; i++) { // sub is the end of the array
+        playersArray[i].printStats();
+    }
+    playersArray[2].printStats();
+}
+
+function printTeam() {
+    console.log("= = = = Team Stats = = = =");
+    for (let i = 0; i < playersArray.length - 1; i++) { // sub is the end of the array
+        playersArray[i].printStats();
+    }
+    playersArray[2].printStats();
+    console.log("= = = = = = = = = = = = = =");
+
 }
 
 makeAPlayer();
 
 /*
-  * Run 5 times. Each time the function runs:
-    * Two random numbers between 1 and 20 are rolled and compared against the starters' offensive and defensive stats
-      * If the first number is lower than the sum of the team's offensive stat, add one point to the team's score.
-      * If the second number is higher than the sum of the team's defensive stat, remove one point from the team's score.
-    * After the score has been changed, prompt the user to allow them to substitute 1 player from within the starters array with the player from within the subs array.
+
   * After the game has finished (been run 5 times):
     * If the score is positive, run `goodGame` for all of the players currently within the starters array.
     * If the score is negative, run `badGame` for all of the players currently within the starters array.
@@ -132,10 +202,6 @@ makeAPlayer();
     * After printing the results, ask the user if they would like to play again.
       * Run `playGame` from the start once more if they do.
       * End the program if they don't.
-
-* HINT: Remember to use recursion when looping with inquirer! Otherwise your program might not return the prompts as you expect.
-
-* HINT: It has been a while since we have worked with random numbers explicitly. If you are having troubles, look up Math.random on Google and you should find some resources to help.
 
 
 */
